0dffc9ddd5783fafe15f58b19ad4c5f16e729615
Max Tsao <mt9304@gmail.com>
2017-08-15 21:13:37 -0700

    After using other formula.

--- src/com/mtdev/autonmz/StartClick.java
+++ src/com/mtdev/autonmz/StartClick.java
@@ -1,5 +1,6 @@
 package com.mtdev.autonmz;
 
+import java.awt.AWTException;
 import java.awt.MouseInfo;
 import java.awt.Point;
 import java.awt.PointerInfo;
@@ -68,7 +69,7 @@ public class StartClick implements Runnable
 			int fromX = (int) currentPos.getX();
 			int fromY = (int) currentPos.getY();
 			
-			GlideMouse(1000,500, fromX, fromY);
+			GlideMouse(fromX,fromY, 1000, 500, 5000, 1000);
 
 			autoClicker.setProgressState(true);
 			JOptionPane.showMessageDialog(null, "Done.");
@@ -78,135 +79,19 @@ public class StartClick implements Runnable
 		}
 	}
 	
-	public void GlideMouse(int toX, int toY, int fromX, int fromY)
+	public void GlideMouse(int x1, int y1, int x2, int y2, int t, int n)
 	{
-		boolean XShouldMove = true;
-		boolean YShouldMove = true;
-		
-		int xDif = toX - fromX;
-		int yDif = toY - fromY;
-
-		double xyDifTally = 0;
-		double yxDifTally = 0;
-		
-		//float xyDif = Math.abs(xDif)/Math.abs(yDif);
-		//float yxDif = Math.abs(yDif)/Math.abs(xDif);
-		
-		//Remember to take care of divide by zero. 
-		//float xyDif = Math.abs(xDif/yDif);
-		//float yxDif = Math.abs(yDif/xDif);
-		
-		float xyDif = (Math.abs(xDif) * 100.0f) / Math.abs(yDif);
-		float yxDif = (Math.abs(yDif) * 100.0f) / Math.abs(xDif);
-		
-		System.out.println("Y: " + yDif);
-		System.out.println("X: " + xDif);
-		
-		try
-		{
-			while (!XYReached)
-			{
-				int rxint = randomGenerator.nextInt(3 + 1 + -1) - 1;
-				int ryint = randomGenerator.nextInt(3);
-				currentMouse = MouseInfo.getPointerInfo();
-				currentPos = currentMouse.getLocation();
-				currentX = (int) currentPos.getX();
-				currentY = (int) currentPos.getY();
-				
-				int currentXDif = toX - currentX;
-				int currentYDif = toY - currentY;
-				
-				yxDifTally += yxDif;
-				xyDifTally += xyDif;
-				
-				System.out.println("YX: " + yxDifTally);
-				System.out.println("XY: " + xyDifTally);
-				/*
-				if (xyDifTally > 1)
-				{
-					YShouldMove = true;
-				}
-				else
-				{
-					YShouldMove = false;
-				}
-				
-				if (yxDifTally > 1)
-				{
-					XShouldMove = true;
-				}
-				else
-				{
-					XShouldMove = false;
-				}
-				*/
-				
-				
-				//Add an arc to it. 
-				//Add random RNG +1 or +2 pixels for speed. Should be faster in middle xDif/2 and yDif/2
-				//Moves to the destination by incrementing by 1*negative or positive difference. 
-				if (xyDifTally > 100)
-				{
-					robot.mouseMove((int)(currentX+(2*Math.signum(currentXDif))), currentY);
-					xyDifTally = 0;
-				}
-				
-				if (yxDifTally > 100)
-				{
-					robot.mouseMove(currentX, (int)(currentY+(2*Math.signum(currentYDif))));
-					yxDifTally = 0;
-				}
-				
-				int randomInt1 = randomGenerator.nextInt(2 + 1 + -1) - 1;
-				//System.out.println(randomInt1);
-				int randomInt2 = randomGenerator.nextInt(2 + 1 + -1) - 1;
-				
-
-				
-				//robot.mouseMove((int)(currentX+(randomInt1*Math.signum(xDif))), (int)(currentY+(randomInt2*Math.signum(yDif))));
-				
-
-				
-				//Helps Check for two conditions. Remember to condense and use ? to check instead. 
-				if ((currentX > toX - 20 && currentX < toX + 20))
-				{
-					XNotReached=false;
-				}
-				else
-				{
-					XNotReached=true;
-					XYReached=false;
-				}
-				
-				if ((currentY > toY - 20 && currentY < toY + 20))
-				{
-					YNotReached=false;
-				}
-				else
-				{
-					YNotReached=true;
-					XYReached=false;
-				}
-				
-				if (!XNotReached && !YNotReached)
-				{
-					XYReached = true;
-				}
-				
-				
-				int randomInt = randomGenerator.nextInt(15);
-				if (randomInt > 10)
-				{
-					randomInt = 1;
-				}
-	
-				Thread.sleep(randomInt);
-			}
-			
-		} catch (InterruptedException e)
-		{
-			e.printStackTrace();
-		}
+	    try {
+	        double dx = (x2 - x1) / ((double) n);
+	        double dy = (y2 - y1) / ((double) n);
+	        double dt = t / ((double) n);
+	        for (int step = 1; step <= n; step++) {
+	            Thread.sleep((int) dt);
+	            robot.mouseMove((int) (x1 + dx * step), (int) (y1 + dy * step));
+	        }
+	    } catch (InterruptedException e) {
+	        e.printStackTrace();
+	    }
 	}
 	
 	
