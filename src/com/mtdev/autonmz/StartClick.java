2d8875280b483b8b99b965c6faea8dd933e9a884
Max Tsao <mt9304@gmail.com>
2017-09-12 18:11:14 -0700

    Finished declaring 3 dimensional arrays for inv.

--- src/com/mtdev/autonmz/StartClick.java
+++ src/com/mtdev/autonmz/StartClick.java
@@ -23,9 +23,43 @@ public class StartClick implements Runnable
 	int currentX = (int) currentPos.getX();
 	int currentY = (int) currentPos.getY();
 	Random randomGenerator = new Random();
-	boolean XNotReached=true;
-	boolean YNotReached=true;
-	boolean XYReached=false;
+	boolean XNotReached = true;
+	boolean YNotReached = true;
+	boolean XYReached = false;
+	
+	/*
+	1223 403
+	1266 438
+	1309 474
+	1350 510
+	*/
+	
+	//Pray button. 
+	int pb[] = {1202,233};
+	//First slot. 
+	int fs[] = {pb[0]+21, pb[1]+170};
+	//Difference between slots. 
+	int invd[] = {43,25};
+	
+	//Not initiated with loop since each value will have to be individually changed later. Each slot is its own space and will need its own xy value just a bit off center. Maybe look into adding RNG in loop. 
+	int invA[][][] = 
+	{
+		{{fs[0],fs[1]},				{fs[0]+invd[0],fs[1]},				{fs[0]+(invd[0]*2), fs[1]},				{fs[0]+(invd[0]*3), fs[1]}},
+		{{fs[0],fs[1]+invd[1]},		{fs[0]+invd[0],fs[1]+invd[1]},		{fs[0]+(invd[0]*2), fs[1]+invd[1]},		{fs[0]+(invd[0]*3), fs[1]+invd[1]}},
+		{{fs[0],fs[1]+(invd[1]*2)},	{fs[0]+invd[0],fs[1]+(invd[1]*2)},	{fs[0]+(invd[0]*2), fs[1]+(invd[1]*2)},	{fs[0]+(invd[0]*3), fs[1]+(invd[1]*2)}},
+		{{fs[0],fs[1]+(invd[1]*3)},	{fs[0]+invd[0],fs[1]+(invd[1]*3)},	{fs[0]+(invd[0]*2), fs[1]+(invd[1]*3)},	{fs[0]+(invd[0]*3), fs[1]+(invd[1]*3)}}
+
+	};
+	
+	int invO[][][] =
+	{
+		
+		{{fs[0],fs[1]+(invd[1]*4)},	{fs[0]+invd[0],fs[1]+(invd[1]*4)},	{fs[0]+(invd[0]*2), fs[1]+(invd[1]*4)},	{fs[0]+(invd[0]*3), fs[1]+(invd[1]*4)}},
+		{{fs[0],fs[1]+(invd[1]*5)},	{fs[0]+invd[0],fs[1]+(invd[1]*5)},	{fs[0]+(invd[0]*2), fs[1]+(invd[1]*5)}, {fs[0]+(invd[0]*3), fs[1]+(invd[1]*5)}}
+	};
+	
+
+	
 	
 	public StartClick(AutoClicker autoClicker, int nClick, int interval)
 	{
@@ -41,8 +75,6 @@ public class StartClick implements Runnable
 			e.printStackTrace();
 		}
 	}
-	
-	
 
 	@Override
 	public void run()
@@ -50,26 +82,54 @@ public class StartClick implements Runnable
 		try
 		{
 			autoClicker.setProgressState(false);
-			Thread.sleep(3000);
-			
-			
-/*
-			for (int i = 0; i < nClick; i++)
-			{
-				robot.mousePress(InputEvent.BUTTON1_MASK);
-				robot.mouseRelease(InputEvent.BUTTON1_MASK);
-				Thread.sleep(interval);
-			}
-			
-			robot.keyPress(KeyEvent.VK_1);
-			robot.keyRelease(KeyEvent.VK_1);
-*/
+			Thread.sleep(1500);
+
+			/*
+			 * for (int i = 0; i < nClick; i++) {
+			 * robot.mousePress(InputEvent.BUTTON1_MASK);
+			 * robot.mouseRelease(InputEvent.BUTTON1_MASK);
+			 * Thread.sleep(interval); }
+			 * 
+			 * robot.keyPress(KeyEvent.VK_1); robot.keyRelease(KeyEvent.VK_1);
+			 */
 			currentMouse = MouseInfo.getPointerInfo();
 			currentPos = currentMouse.getLocation();
 			int fromX = (int) currentPos.getX();
 			int fromY = (int) currentPos.getY();
+
+			for(int i=0;i<invA.length;i++)
+			{
+				
+				for(int j=0;j<invA[0].length;j++)
+				{
+					System.out.print("{ ");
+					for(int k=0;k<invA[0][1].length;k++)
+					{
+						System.out.print(invA[i][j][k]+" ");
+					}
+					System.out.print("} ");
+				}
+				System.out.println(" ");
+			}
 			
-			GlideMouse(fromX,fromY, 1000, 500, 5000, 1000);
+			System.out.println(" ");
+			
+			for(int i=0;i<invO.length;i++)
+			{
+				
+				for(int j=0;j<invO[0].length;j++)
+				{
+					System.out.print("{ ");
+					for(int k=0;k<invO[0][1].length;k++)
+					{
+						System.out.print(invO[i][j][k]+" ");
+					}
+					System.out.print("} ");
+				}
+				System.out.println(" ");
+			}
+			
+			//GlideMouse(fromX, fromY, 1000, 500, 1000, 1000);
 
 			autoClicker.setProgressState(true);
 			JOptionPane.showMessageDialog(null, "Done.");
@@ -78,23 +138,23 @@ public class StartClick implements Runnable
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void GlideMouse(int x1, int y1, int x2, int y2, int t, int n)
 	{
-	    try {
-	        double dx = (x2 - x1) / ((double) n);
-	        double dy = (y2 - y1) / ((double) n);
-	        double dt = t / ((double) n);
-	        for (int step = 1; step <= n; step++) {
-	            Thread.sleep((int) dt);
-	            robot.mouseMove((int) (x1 + dx * step), (int) (y1 + dy * step));
-	        }
-	    } catch (InterruptedException e) {
-	        e.printStackTrace();
-	    }
+		try
+		{
+			double dx = (x2 - x1) / ((double) n);
+			double dy = (y2 - y1) / ((double) n);
+			double dt = t / ((double) n);
+			for (int step = 1; step <= n; step++)
+			{
+				Thread.sleep((int) dt);
+				robot.mouseMove((int) (x1 + dx * step), (int) (y1 + dy * step));
+			}
+		} catch (InterruptedException e)
+		{
+			e.printStackTrace();
+		}
 	}
-	
-	
-	
 
 }
